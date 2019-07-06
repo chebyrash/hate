353ad019e875893a1afb9e9f43632b824ba3316e
chebyrash <pd.stroganov@gmail.com>
2019-07-06 17:08:55 +0100

    Added build files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker rmi chebyrash/channy-hate
+docker build -t chebyrash/channy-hate .
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker kill $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -aq)
+
+docker run -d -p 80:80 --restart=unless-stopped --log-opt max-size=10m --log-opt mode=non-blocking chebyrash/channy-hate
